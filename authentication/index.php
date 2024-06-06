b720ef235f400702dda26860396e807f3a4108a2
PuttipongMax <maxnetoogo@gmail.com>
2024-06-06 15:48:35 +0700

    faet: Setup project header & footer and homePage

---
+++ authentication/index.php
@@ -0,0 +1,26 @@
+<?php
+  include "layout/header.php";
+?>
+
+    <div style="background-color: #08618d;">
+      <div class="container text-white py-5">
+        <div class="row align-items-center g-5">
+          <div class="col-md-6">
+            <h1 class="mb-5 display-2">
+              <strong>Best Shop of Electronics</strong>
+            </h1>
+            <p>
+              Find a large selection of newest electronic devices 
+              from most popular brands and with affordable prices.
+            </p>
+          </div>
+          <div class="col mb-6 text-center">
+            <img src="<?php echo $base_url; ?>/images/05.jpg" class="img-fluid" alt="hero" />
+          </div>
+        </div>
+      </div>
+    </div>
+
+<?php 
+  include "layout/footer.php";
+?>
---
+++ authentication/layout/footer.php
@@ -0,0 +1,20 @@
+<footer class="py-5" style="background-color: #eef0f2;">
+      <div class="container">
+        <div class="row">
+          <div class="col text-center">
+            <img  
+              src="<?php echo $base_url; ?>/images/ghost.png" 
+              class="mb-2" width="24" height="24"
+            />
+            <small class="d-block tex-muted">
+              <!-- shorthand -->
+              &copy; 2017-<?= date("Y") ?> 
+            </small>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
