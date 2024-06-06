26f765b1d4f3bdb72ba5ac113b177d97365f0562
PuttipongMax <maxnetoogo@gmail.com>
2024-06-06 22:05:22 +0700

    feat: ProfilePage.php and get $_SESSION data implements show profile

---
+++ authentication/profile.php
@@ -0,0 +1,48 @@
+<?php
+ include "layout/header.php";
+
+ // Check if the user is logged in, if not then redirect him to login page
+ if(!isset($_SESSION["email"])){
+  header("location: ". $base_url. "/index.php");
+  exit;
+ }
+?>
+
+<div class="container py-5">
+ <div class="row">
+  <div class="col-lg-6 mx-auto border shadow p-4">
+   <h2 class="text-center mb-4">Profile</h2>
+   <hr />
+
+   <div class="row mb-3">
+    <div class="col-sm-4">First Name</div>
+    <div class="col-sm-8"><?php echo $_SESSION["first_name"] ?></div>
+   </div>
+   <div class="row mb-3">
+    <div class="col-sm-4">Last Name</div>
+    <div class="col-sm-8"><?php echo $_SESSION["last_name"] ?></div>
+   </div>
+   <div class="row mb-3">
+    <div class="col-sm-4">Email</div>
+    <div class="col-sm-8"><?php echo $_SESSION["email"] ?></div>
+   </div>
+   <div class="row mb-3">
+    <div class="col-sm-4">Phone</div>
+    <div class="col-sm-8"><?php echo $_SESSION["phone"] ?></div>
+   </div>
+   <div class="row mb-3">
+    <div class="col-sm-4">Address</div>
+    <div class="col-sm-8"><?php echo $_SESSION["address"]; ?></div>
+   </div>
+   <div class="row mb-3">
+    <div class="col-sm-4">Registered</div>
+    <div class="col-sm-8"><?php echo $_SESSION["created_at"]; ?></div>
+   </div>
+
+  </div>
+ </div>
+</div>
+
+<?php
+ include "layout/footer.php";
+?>
